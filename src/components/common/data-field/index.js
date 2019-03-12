be7f0ecb0cc9e0beedd1466119725039a31cafdf
fadi-saadeldin <fadi.saadeldin@gmail.com>
2019-03-13 01:16:41 +0200

    Cars query project

--- src/components/common/data-field/index.js
+++ src/components/common/data-field/index.js
@@ -1,7 +1,3 @@
-// Copyright (C) Sukoon - All Rights Reserved
-// Unauthorized copying of this file, via any medium is strictly prohibited
-// Proprietary and confidential. Authored by Karim Agha <karim.dev@gmail.com>
-
 import React from 'react';
 
 import DropDownField from './dropdown';
